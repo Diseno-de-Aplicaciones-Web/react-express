2518101fac17309a9cdfe0e6b49b6b0be7e53a2d
DanBaDo <byte4life@pm.me>
2023-03-15 19:50:28 +0100

    Turns things easy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,13 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [datos, setDatos] = useState([])
-  const [listaTarefas, setListaTarefas] = useState([])
-  const [procesoActual, setProcesoActual] = useState("Iniciando aplicación")
-
-  useEffect(
-    actualizaDatos,
-    []
-  )
 
   useEffect(
     ()=>{
-      const novaLista = datos.map(HTMLparaTarefa)
-      setListaTarefas(novaLista)
+      fetch("http://localhost:8000/tarefa/").then(reaccionParaResposta)
     },
-    [datos]
+    []
   )
-
-  function actualizaDatos(){
-    setProcesoActual("Obtendo a lista de tarefas")
-    fetch("http://localhost:8000/tarefa/")
-      .then(reaccionParaResposta)
-      .catch(reaccionParaErrorAccedendoDatos)
-  }
 
   function reaccionParaResposta(resposta){
     resposta.json().then(reaccionParaNovosDatos)
@@ -35,26 +20,14 @@ function App() {
     setDatos(novosDatos)
   }
 
-  function reaccionParaErrorAccedendoDatos(error){
-    setDatos([])
-    setProcesoActual("Non foi posible obter as tarefas. Reintentando nuns segundos.")
-    setTimeout(actualizaDatos,2000)
-  }
-
-  function HTMLparaTarefa(tarefa){
-    return(
-      <label key={tarefa.id}>
-        {tarefa.descripcion}
-        <input type="checkbox" checked={tarefa.rematada}/>
-      </label>
-    )
-  }
-
   return (
     <main>
       <h1>React - Express</h1>
-      { datos.length === 0 && <p>{procesoActual}</p>}
-      { datos.length > 0 && listaTarefas}
+      {
+        datos.map(
+          tarefa=><p key={tarefa.id}>{tarefa.descripcion} <input type="checkbox" checked={tarefa.rematada}/></p>
+        )
+      }
     </main>
   );
 
